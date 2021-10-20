59dff63ea0a7d035db37352df111ece1d7565fcd
Tom Werkheiser <tom.werkheiser@crompco.com>
2021-10-20 15:05:24 -0400

    Initial commit

---
+++ pup.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer-core');
+const {Command} = require('commander');
+const program = new Command();
+
+program
+    .argument('<input-path>', 'Path for the HTML file')
+    .argument('<output-path>', 'Path for the PDF file to be saved')
+    .option('--page-size <type>', "Page size, normal print paper sizes", 'letter')
+    .option('--height <type>', 'Height of browser window viewport', '1024')
+    .option('--width  <type>', 'Width of browser window viewport', '768')
+    .option('--print-background <type>', 'Whether the background should be in the pdf', true)
+    .option('--emulate-type <type>', "The type of CSS media type of the page. 'screen', 'print', 'null'", false)
+    .allowUnknownOption()
+
+program.parse(process.argv);
+
+const flags = program.opts();
+const [inputPath, outputPath] = program.processedArgs;
+
+(async () => {
+    try {
+        const input = inputPath;
+        const output = outputPath;
+
+        const browser = await puppeteer.launch({
+            defaultViewport: {
+                width: parseFloat(flags.width),
+                height: parseFloat(flags.height),
+            },
+            executablePath: 'google-chrome-stable',
+            args: ['--no-sandbox']
+        });
+        const page = await browser.newPage();
+        const file = `file://${input}`;
+
+        if ( flags.emulateType !== false ) {
+            await page.emulateMediaType(flags.emulateType);
+        }
+
+        await page.goto(file);
+
+        await page.pdf({
+            format: flags.pageSize,
+            path: output,
+            printBackground: flags.printBackground,
+            width: parseFloat(flags.width),
+            height: parseFloat(flags.height),
+        });
+
+        await browser.close();
+    } catch ( e ) {
+        console.log(e);
+    }
+})();
