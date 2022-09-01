510df117e53d0563e933b57451b4619ad3bc33e0
Erich Oelschlegel <erich.oelschlegel@crompco.com>
2022-09-01 10:01:36 -0400

    add command line support for pdf margins

--- pup.js
+++ pup.js
@@ -6,6 +6,11 @@ program
     .argument('<input-path>', 'Path for the HTML file')
     .argument('<output-path>', 'Path for the PDF file to be saved')
     .option('--page-size <type>', "Page size, normal print paper sizes", 'letter')
+    .option('--margin <value>', "The margin used when generating a PDF e.g. '0.5in'", 0)
+    .option('--margin-top <value>', "The top margin used when generating a PDF e.g. '0.5in'", 0)
+    .option('--margin-left <value>', "The left margin used when generating a PDF e.g. '0.5in'", 0)
+    .option('--margin-right <value>', "The right margin used when generating a PDF e.g. '0.5in'", 0)
+    .option('--margin-bottom <value>', "The bottom margin used when generating a PDF e.g. '0.5in'", 0)
     .option('--height <type>', 'Height of browser window viewport', '1024')
     .option('--width  <type>', 'Width of browser window viewport', '768')
     .option('--print-background <type>', 'Whether the background should be in the pdf', true)
@@ -38,13 +43,19 @@ const [inputPath, outputPath] = program.processedArgs;
         }
 
         await page.goto(file);
-
+        
         await page.pdf({
             format: flags.pageSize,
             path: output,
             printBackground: flags.printBackground,
             width: parseFloat(flags.width),
             height: parseFloat(flags.height),
+            margin: {
+                top: flags.marginTop || flags.margin,
+                left: flags.marginLeft || flags.margin,
+                right: flags.marginRight || flags.margin,
+                bottom: flags.marginBottom || flags.margin,
+            },
         });
 
         await browser.close();
